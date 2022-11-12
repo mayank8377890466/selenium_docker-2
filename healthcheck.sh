9ac2366c63bbc19de29699f9c768068607db765c
lucky-mishra <lucky1985.mayank@gmail.com>
2022-11-12 21:29:55 +0530

    change image name in docker compose file

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,7 +6,7 @@
 
 echo "Checking if hub is ready "
 
-while [ "$( curl -s http://172.26.0.1:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://172.26.96.1:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 	sleep 1
 done
